07f646d966756dec8c5d44f241d86ae9ce8a75fd
CesarABaron <127266668+CesarABaron@users.noreply.github.com>
2023-08-24 13:07:14 -0500

    prueba

--- KenController.jsx
+++ KenController.jsx
@@ -1,11 +1,12 @@
-import React from 'react'
+import React from "react";
 
 const KenController = () => {
   return (
     <div>
       <h1>Soy unaprueba</h1>
+      <h2>soy una prueba2</h2>
     </div>
-  )
-}
+  );
+};
 
-export default KenController
+export default KenController;
